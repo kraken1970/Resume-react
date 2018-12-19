62369a2ebf74b9a50b587c183f741a599e267df6
Serg Minin <everestdon@ukr.net>
2018-12-20 01:58:09 +0300

    reframe css

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,23 +5,35 @@ const Contacts = () => {
     <div className="contacts-wrap">
       <h3>Контактная информация:</h3>
       <p>Адрес: г. Донецк</p>
-      <div>
+      <div className="contact-wrap">
         <a href="tel:+380713537644" title="Позвоните мне">
           Телефон: +380713537644
         </a>
       </div>
-      <div>
-        <a href="viber://add?number=+380668031813" title="Звонок через Viber">
+      <div className="contact-wrap">
+        <a
+          href="viber://add?number=+380668031813"
+          title="Звонок через Viber"
+          className="link-viber"
+        >
           Viber: +380668031813
         </a>
       </div>
-      <div>
-        <a href="skype:tabaska14?call" title="Звонок через Skype">
+      <div className="contact-wrap">
+        <a
+          href="skype:tabaska14?call"
+          title="Звонок через Skype"
+          className="link-skype"
+        >
           Skype: tabaska14
         </a>
       </div>
-      <div>
-        <a href="mailto:S.V.Minin@gmail.com" title="Напишите мне">
+      <div className="contact-wrap">
+        <a
+          href="mailto:S.V.Minin@gmail.com"
+          title="Напишите мне"
+          className="link-mail"
+        >
           E-mail: S.V.Minin@gmail.com
         </a>
       </div>
